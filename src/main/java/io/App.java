896829c417e06bdbce7609e806c18ffe552c20c7
Marcin Bialek <marcin.bialek@live.com>
2022-05-23 13:52:38 +0200

    1. Jetty embeded setup done. Works OK

--- src/main/java/io/App.java
+++ src/main/java/io/App.java
@@ -1,10 +1,35 @@
 package io;
 
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.webapp.*;
 import org.slf4j.LoggerFactory;
 
 public class App {
-    public static void main(String[] args) {
-            var logger = LoggerFactory.getLogger(App.class);
-            logger.info("Hello App.class");
+    public static void main(String[] args) throws Exception {
+        var webapp = new WebAppContext();
+        webapp.setResourceBase("src/main/webapp");
+        webapp.setContextPath("/");
+        webapp.setConfigurations(new Configuration[]
+                {
+                        new AnnotationConfiguration(),
+                        new WebInfConfiguration(),
+                        new WebXmlConfiguration(),
+                        new MetaInfConfiguration(),
+                        new FragmentConfiguration(),
+                        new EnvConfiguration(),
+                        new PlusConfiguration(),
+                        new JettyWebXmlConfiguration(),
+                        new WebAppConfiguration() // needed for Jetty 11
+                });
+                webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
+        var server = new Server(8080);
+        server.setHandler(webapp);
+
+        server.start();
+        server.join();
     }
 }
--- src/main/java/io/HelloServlet.java
+++ src/main/java/io/HelloServlet.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello Servlet");
+        {
+            resp.getWriter().println("Hello Servlet");
+        }
     }
 }
